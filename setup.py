6034c815fadac956abfba6f3d53c1fb7b025892e
xrotwang <xrotwang@googlemail.com>
2015-12-18 13:20:15 +0100

    bumbed version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name='pycdstar',
-    version="0.1.1",
+    version="0.1.2",
     description='A python wrapper for the CDStar API',
     long_description=read("README.md"),
     author='Robert Forkel',
