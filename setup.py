e8e00812ed83e836a6e5e367a81948e82f03831d
xrotwang <xrotwang@googlemail.com>
2018-06-12 09:22:43 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycdstar',
-    version="0.4.0",
+    version="0.4.1.dev0",
     description='A python wrapper for the CDStar API',
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
